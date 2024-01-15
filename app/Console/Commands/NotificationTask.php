994001574776dff18e37467faa46af6f3e1e8104
N'Faly KABA <nfaly.kaba@gmail.com>
2024-01-15 16:40:39 -0500

    resending process

--- app/Console/Commands/NotificationTask.php
+++ app/Console/Commands/NotificationTask.php
@@ -44,6 +44,8 @@ class NotificationTask extends Command
     {
 
         $this->sendNewNotifications();
+        $this->updateMemberNotifications();
+        $this->updateWalyNotifications();
 
         $this->info('Notification task executed successfully!');
     }
@@ -58,19 +60,14 @@ class NotificationTask extends Command
         $payments_notifications_ids = DB::table('VbE_custom_payments_notifications')->select('entry_id');
         $notifications = DB::table('VbE_view_wpforms_payments_done')
             ->whereNotIn('entry_id', $payments_notifications_ids)
+            ->limit(10)
             ->get();
 
         foreach ($notifications as $notification)
         {
             $member_mail_sent_at = null;
             $walynw_mail_sent_at = null;
-            $body = [
-                'name' => $notification->name,
-                'email' => $notification->email,
-                'total_amount' => $notification->total_amount,
-                'date_created_gmt' => $notification->date_created_gmt,
-                'currency' => $notification->currency,
-            ];
+            $body = $this->buidBody($notification);
             $is_production = config('app.env') === 'production';
             $member_mail = $is_production ? $notification->email : config('app.testmail');
 
@@ -92,5 +89,81 @@ class NotificationTask extends Command
             ]);
         }
         Log::info("Sending mails for new payments --> End");
+    }
+
+    private function updateMemberNotifications()
+    {
+        Log::info("Resending mails to member for new payments --> Start");
+
+        $payments_notifications_ids = DB::table('VbE_custom_payments_notifications')
+            ->select('entry_id')
+            ->where('member_mail_sent_at', '=', null);
+
+        $notifications = DB::table('VbE_view_wpforms_payments_done')
+            ->whereIn('entry_id', $payments_notifications_ids)
+            ->limit(10)
+            ->get();
+
+        foreach ($notifications as $notification)
+        {
+            $member_mail_sent_at = null;
+            $is_production = config('app.env') === 'production';
+            $member_mail = $is_production ? $notification->email : config('app.testmail');
+
+            if (Mail::to($member_mail)->send(new NotificationMail($this->buidBody($notification), 'notifications.member-payment-confirmation', 'Confirmation de paiement')))
+            {
+                $member_mail_sent_at = Carbon::now();
+                Log::info("Update - Member Mail sent to {$notification->email}");
+            }
+
+            DB::table('VbE_custom_payments_notifications')
+                ->where('entry_id', '=', $notification->entry_id)
+                ->update(['member_mail_sent_at' => $member_mail_sent_at]);
+        }
+        Log::info("Resending mails to members for new payments --> End");
+    }
+
+    private function updateWalyNotifications()
+    {
+        Log::info("Resending mails to waly for new payments --> Start");
+
+        $payments_notifications_ids = DB::table('VbE_custom_payments_notifications')
+            ->select('entry_id')
+            ->where('walynw_mail_sent_at', '=', null);
+
+        $notifications = DB::table('VbE_view_wpforms_payments_done')
+            ->whereIn('entry_id', $payments_notifications_ids)
+            ->limit(10)
+            ->get();
+
+        foreach ($notifications as $notification)
+        {
+            $walynw_mail_sent_at = null;
+            $is_production = config('app.env') === 'production';
+            $walymail = $is_production ? config('app.walynw_email') : config('app.testmail');
+
+            if (Mail::to($walymail)->send(new NotificationMail($this->buidBody($notification), 'notifications.walynw-payment-notification', 'Notification de paiement')))
+            {
+                $walynw_mail_sent_at = Carbon::now();
+                Log::info("Update - Walynw Mail sent to {$notification->email}");
+            }
+
+
+            DB::table('VbE_custom_payments_notifications')
+                ->where('entry_id', '=', $notification->entry_id)
+                ->update(['walynw_mail_sent_at' => $walynw_mail_sent_at]);
+        }
+        Log::info("Resending mails to waly for new payments --> End");
+    }
+
+    private function buidBody($notification)
+    {
+        return [
+            'name' => $notification->name,
+            'email' => $notification->email,
+            'total_amount' => $notification->total_amount,
+            'date_created_gmt' => $notification->date_created_gmt,
+            'currency' => $notification->currency,
+        ];
     }
 }
