b045c3d07d1001b2d6e0e119766b823a5d669167
N'Faly KABA <nfaly.kaba@gmail.com>
2024-01-21 22:01:56 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-npm run build
-ssh nafadjitech "cd; cd waly-notification; git stash; git pull"
-scp -r public/build nafadjitech:/home/nafaudww/public_html/notification
-scp -r public/css nafadjitech:/home/nafaudww/public_html/notification
-scp -r public/images nafadjitech:/home/nafaudww/public_html/notification
-scp -r public/js nafadjitech:/home/nafaudww/public_html/notification
+# npm run build
+# ssh nafadjitech "cd; cd waly-notification; git stash; git pull"
+# scp -r public/build nafadjitech:/home/nafaudww/public_html/notification
+# scp -r public/css nafadjitech:/home/nafaudww/public_html/notification
+# scp -r public/images nafadjitech:/home/nafaudww/public_html/notification
+# scp -r public/js nafadjitech:/home/nafaudww/public_html/notification
 
 # Waly
-# scp -r public/build waly:/home4/walynetw/public_html/waly-notification
-# scp -r public/css waly:/home4/walynetw/public_html/waly-notification
-# scp -r public/images waly:/home4/walynetw/public_html/waly-notification
-# scp -r public/js waly:/home4/walynetw/public_html/waly-notification
+scp -r public/build waly:/home4/walynetw/public_html/waly-notification
+scp -r public/css waly:/home4/walynetw/public_html/waly-notification
+scp -r public/images waly:/home4/walynetw/public_html/waly-notification
+scp -r public/js waly:/home4/walynetw/public_html/waly-notification
